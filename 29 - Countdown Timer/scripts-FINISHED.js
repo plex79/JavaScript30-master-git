dbad86fb5da1867e1bc3116c102aaa2b901c4019
plex <contact@eplex.eu>
2018-04-11 14:26:33 +0200

    next

--- 29 - Countdown Timer/scripts-FINISHED.js
+++ 29 - Countdown Timer/scripts-FINISHED.js
@@ -14,6 +14,7 @@ function timer(seconds) {
 
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
+    console.log(then, Date.now());
     // check if we should stop it!
     if(secondsLeft < 0) {
       clearInterval(countdown);
@@ -21,24 +22,36 @@ function timer(seconds) {
     }
     // display it
     displayTimeLeft(secondsLeft);
+    
   }, 1000);
 }
 
 function displayTimeLeft(seconds) {
   const minutes = Math.floor(seconds / 60);
   const remainderSeconds = seconds % 60;
+
+
+  let spanNow = document.createElement("span");
+  let spanPrev = document.createElement("span");
+  spanNow.classList.add('now');
+  spanPrev.classList.add('prev');
+
+  spanNow.textContent = remainderSeconds;
+  spanPrev.textContent = remainderSeconds -1 < 0 ? '0' : remainderSeconds -1;
+
+  timerDisplay.appendChild(spanNow);
+  timerDisplay.appendChild(spanPrev);
+
+
   const display = `${minutes}:${remainderSeconds < 10 ? '0' : '' }${remainderSeconds}`;
   document.title = display;
-  timerDisplay.textContent = display;
+
+
+
+  //timerDisplay.textContent = display;
 }
 
-function displayEndTime(timestamp) {
-  const end = new Date(timestamp);
-  const hour = end.getHours();
-  const adjustedHour = hour > 12 ? hour - 12 : hour;
-  const minutes = end.getMinutes();
-  endTime.textContent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
-}
+
 
 function startTimer() {
   const seconds = parseInt(this.dataset.time);
@@ -46,10 +59,7 @@ function startTimer() {
 }
 
 buttons.forEach(button => button.addEventListener('click', startTimer));
-document.customForm.addEventListener('submit', function(e) {
-  e.preventDefault();
-  const mins = this.minutes.value;
-  console.log(mins);
-  timer(mins * 60);
-  this.reset();
-});
+
+
+
+
